db47df080514b1adfb648260646b1fdbcf3ca991
Higor <higor.ruan456@gmail.com>
2025-07-20 17:53:30 -0300

    feat: create logout endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,25 @@
-import { Body, Controller, HttpCode, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthParamsDto, AuthResponseDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import {
   ApiBadRequestResponse,
   ApiBody,
   ApiConflictResponse,
+  ApiCookieAuth,
   ApiCreatedResponse,
   ApiOkResponse,
   ApiOperation,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import { Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -99,5 +108,45 @@ export class AuthController {
     });
 
     return { message: 'User registered successfully' };
+  }
+
+  @Post('logout')
+  @HttpCode(200)
+  @UseGuards(AuthGuard('jwt'))
+  @ApiCookieAuth('access_token')
+  @ApiOperation({
+    summary: 'User logout',
+    description:
+      'Logs out the user by clearing the authentication cookie (access_token).',
+  })
+  @ApiOkResponse({
+    description: 'User successfully logged out. Authentication cookie cleared.',
+    schema: {
+      example: {
+        message: 'User logged out successfully',
+      },
+    },
+    headers: {
+      'Set-Cookie': {
+        description: 'Clears the access_token HTTP-only cookie',
+        schema: {
+          type: 'string',
+          example:
+            'access_token=; HttpOnly; Secure; SameSite=None; Max-Age=0; Path=/',
+        },
+      },
+    },
+  })
+  logout(@Res({ passthrough: true }) res: Response) {
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    res.clearCookie('access_token', {
+      httpOnly: true,
+      secure: isProduction,
+      sameSite: isProduction ? 'none' : 'lax',
+      path: '/',
+    });
+
+    return { message: 'User logged out successfully' };
   }
 }
